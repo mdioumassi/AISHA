86f5193d61d1b02bde15fc9ef922fd27d26ff6bf
Mohamed DIOUMASSI <m.dioumassi@networks-technology.fr>
2017-11-02 14:27:34 +0100

    collection

--- src/InscriptionBundle/Form/MatiereType.php
+++ src/InscriptionBundle/Form/MatiereType.php
@@ -2,9 +2,14 @@
 
 namespace InscriptionBundle\Form;
 
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraint as Assert;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MatiereType extends AbstractType
 {
@@ -13,9 +18,36 @@ class MatiereType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('libelle')
-                ->add('description')
-                ->add('coefficient');
+        $builder->add('libelle', TextType::class, [
+                'attr' => array(
+                    'class' => 'w3-input w3-border',
+                    'placeholder' => 'Libellé'
+                ),
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(
+                        array('message' => 'Il faut choisir un libellé')
+                    )
+                ],
+                ])
+                ->add('description', TextareaType::class, [
+                    'attr' => array(
+                        'class' => 'w3-input w3-border',
+                        'placeholder' => 'Description'
+                    )
+                 ])
+                ->add('coefficient', TextType::class, [
+                    'constraints' => array(
+                        new NotBlank(
+                            array('message' => 'Il faut choisir un libellé')
+                        )
+                    ),
+                    'attr' => array(
+                        'class' => 'w3-input w3-border',
+                        'placeholder' => 'Coefficient'
+                    ),
+                    'label' => false
+                ]);
                 //->add('niveau');
     }
     
